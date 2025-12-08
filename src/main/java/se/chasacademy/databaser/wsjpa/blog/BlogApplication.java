ec62fac3724e6a84143c77d4a815482dade6ed85
Johan Romin <johan.romin@greycode.se>
2025-12-08 15:06:35 +0100

    Startskal för en Spring Boot applikation med JPA.

---
+++ src/main/java/se/chasacademy/databaser/wsjpa/blog/BlogApplication.java
@@ -0,0 +1,36 @@
+package se.chasacademy.databaser.wsjpa.blog;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import se.chasacademy.databaser.wsjpa.blog.models.Post;
+import se.chasacademy.databaser.wsjpa.blog.repositories.PostRepository;
+
+import java.util.Optional;
+
+@SpringBootApplication
+public class BlogApplication implements CommandLineRunner {
+	private PostRepository postRepository;
+
+	public BlogApplication(PostRepository postRepository) {
+		this.postRepository = postRepository;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		for (Post current : postRepository.findAll()) {
+			System.out.println("title: "+current.getTitle()+" published at: "+current.getPublishDate());
+		}
+
+		Optional<Post> firstPost = postRepository.findById(1L);
+		if (firstPost.isPresent()) {
+			Post first = firstPost.get();
+			System.out.println("title: " + first.getTitle() + " published at: " + first.getPublishDate());
+		}
+	}
+}
